0fb4f3c3aa8a6ff12445c443ec2dcf75d0c8bc0e
RuiJ72 <rlagos900@gmail.com>
2022-10-14 00:13:05 -0300

    Una puerta añadida

---
+++ EjercicioCoche/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System;
+using System.Collections;
+
+namespace EjercicioCoche
+{   
+
+    class Program
+    {   
+        
+
+        static void Main(string[] args)
+        {
+            ArrayList puertas = new ArrayList();
+
+            puertas.Add("1");
+            
+
+            for (int i = 0; i < puertas.Count; i++)
+            {
+                Console.WriteLine("Fué añadida {0} puerta al coche", puertas[i]);
+            }
+
+
+        }
+    }
+}
+
